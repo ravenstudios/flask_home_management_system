bda093ed5ea68d810cf60685a2645691923ea83b
ravenstudios <ravenstudios@Robs-MacBook-Pro.local>
2024-04-12 17:43:52 -0500

    Finished shopping list

--- app/models/shopping_list_model.py
+++ app/models/shopping_list_model.py
@@ -7,20 +7,21 @@ class Shopping_list_item(db.Model):
     __tablename__ = "shopping_list"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    notes = db.Column(db.String(300))
     date_entered = db.Column(db.Date())
+    save_for_later = db.Column(db.Boolean())
+    is_priority = db.Column(db.Boolean())
 
 
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    # user = db.relationship("User", back_populates="shopping_list")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="shopping_list")
 
 
 
     def __init__(self, item):
         self.name = item["name"][0]
-        self.notes = item["notes"][0]
         self.date_entered = datetime.datetime.now()
-
+        self.is_priority = False
+        self.save_for_later = False
 
     def __repr__(self):
         item ={
--- app/shopping_list/shopping_list_blueprint.py
+++ app/shopping_list/shopping_list_blueprint.py
@@ -14,7 +14,7 @@ shopping_list_blueprint = Blueprint('shopping_list_blueprint', __name__, templat
 @shopping_list_blueprint.route('/')
 @login_required
 def index():
-    # return "<h1>shopping_list<?h1>"
+
     return render_template('shopping_list/show_items.html', items=Shopping_list_item.query.all(), title="Shopping List Items")
 
 
@@ -22,7 +22,7 @@ def index():
 @shopping_list_blueprint.route('add-new-item-form', methods = ['GET', 'POST'])
 @login_required
 def add_new_item_form():
-    return render_template('shopping_list/add_new_item_form.html', title="Add New Shopping List Item")
+    return render_template('shopping_list/add_new_item_form.html', title="Add New Shopping List Item", users = User.query.all())
 
 
 
@@ -32,15 +32,54 @@ def add_new_item_form():
 def add_new_item():
     form_data = request.form.to_dict(flat=False)
     item = Shopping_list_item(form_data)
+
+
+    user = User.query.filter_by(name=form_data["user"][0]).first()
+    item.user_id = user.id
+
+
+
     db.session.add(item)
     db.session.commit()
     return redirect("/shopping-list")
 
 
-@shopping_list_blueprint.route('delete-item', methods = ['GET', 'POST'])
+@shopping_list_blueprint.route('delete-items', methods = ['GET', 'POST'])
 @login_required
-def delete_item():
+def delete_items():
     args = request.args
+    ids_string = args.get("ids")
+
+    ids = list(map(int, ids_string.split(',')))
+
+    # Delete rows with IDs in the list
+    db.session.query(Shopping_list_item).filter(Shopping_list_item.id.in_(ids)).delete(synchronize_session=False)
+
+
+
     Shopping_list_item.query.filter_by(id=args.get("id")).delete()
+    db.session.commit()
+    return redirect("/shopping-list")
+
+
+@shopping_list_blueprint.route('save-for-later', methods = ['GET', 'POST'])
+@login_required
+def save_for_later():
+    args = request.args
+    item = Shopping_list_item.query.get(args.get("id"))
+    item.save_for_later = True if args.get("state") == "True" else False
+    db.session.add(item)
+    db.session.commit()
+    return redirect("/shopping-list")
+
+
+
+@shopping_list_blueprint.route('is-priority', methods = ['GET', 'POST'])
+@login_required
+def is_priority():
+    args = request.args
+    item = Shopping_list_item.query.get(args.get("id"))
+    item.is_priority = True if args.get("state") == "True" else False
+    db.session.add(item)
     db.session.commit()
     return redirect("/shopping-list")
